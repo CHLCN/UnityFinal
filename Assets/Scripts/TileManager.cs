4f3ffeff4310c98f46b4d6a339e03511c37c10cf
CHLCN <chaihailun2012@126.com>
2020-12-16 17:53:38 +0800

    import models & create infinite tiles

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0; // 记录道路总长度
+    public float tileLength = 30; // 一段跑道的长度
+    public int numberOfTiles = 5; // 初始生成跑道的数量
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //// 随机生成跑道
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            // 保证生成的第一端路没有路障
+            if (i == 0)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 无尽生成跑道
+        // 如果到当前跑道尽头则重新生成跑道并将之前的跑道删除
+        if (playerTransform.position.z - 35 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    // 生成跑道
+    // 并记录跑道总长度
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go); // 记录现在所用的跑道
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
