69cb265ff39dd55a1994df2dccbb888959681262
CHLCN <chaihailun2012@126.com>
2020-12-25 02:21:19 +0800

    coins & sound

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //金币旋转效果
+        transform.Rotate(50 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //s收集的金币数
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<AudioManager>().PlaySound("PickUpCoin");
+            PlayerManager.numberOfCoins += 1;
+            Destroy(gameObject);
+        }
+    }
+}
